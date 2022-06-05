7fcc11965b602c8587637a8fcb205500b2df1254
vanotis720 <vanotis720@gmail.com>
2022-06-05 13:09:08 +0200

    auth state in react navigation, refactoring needed

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
+import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import AppBottom from './src/navigation/AppBottom';
 import StartedStack from './src/navigation/StartedStack';
 
+
 export default function App() {
+	const [state, setState] = React.useState(false);
+
+	React.useEffect(() => {
+		// Fetch the token from storage then navigate to our appropriate place
+		const bootstrapAsync = async () => {
+			let userName;
+
+			try {
+				userName = await AsyncStorage.getItem('username');
+				setState(true);
+			} catch (e) {
+				// Restoring token failed
+			}
+		};
+
+		bootstrapAsync();
+	}, []);
+
+
 	return (
 		<NavigationContainer>
-			{/* <StartedStack /> */}
-			<AppBottom />
+			{state == false ? (
+				<StartedStack />
+			) : (
+				<AppBottom />
+			)}
 		</NavigationContainer>
 	);
 }
--- src/components/page/Register.js
+++ src/components/page/Register.js
@@ -1,10 +1,71 @@
-import { StatusBar, StyleSheet, Text, View, TouchableOpacity, Image, TextInput } from 'react-native';
+import React, { useState } from "react";
+import axios from "axios";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { StatusBar, StyleSheet, Text, View, TouchableOpacity, Image, TextInput, ActivityIndicator } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import colors from '../../theme/colors';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
+const baseUrl = "https://reqres.in";
+
 
 function Register({ navigation }) {
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+    const onChangeNameHandler = (username) => {
+        setUsername(username);
+    };
+
+    const onChangeEmailHandler = (email) => {
+        setEmail(email);
+    };
+
+    const onChangePasswordHandler = (password) => {
+        setPassword(password);
+    };
+
+    const storeData = async (username) => {
+        try {
+            await AsyncStorage.setItem('username', username)
+        } catch (e) {
+            throw new Error("An error has occurred with storage");
+        }
+    }
+
+    const onSubmitFormHandler = async (event) => {
+        if (!username.trim() || !email.trim() || !password.trim()) {
+            alert("Name or Email is invalid");
+            return;
+        }
+        setIsLoading(true);
+        try {
+            const response = await axios.post(`${baseUrl}/api/users`, {
+                username,
+                email,
+                password
+            });
+            if (response.status === 201) {
+                alert(` You have created: ${JSON.stringify(response.data)}`);
+                setIsLoading(false);
+                setUsername('');
+                setEmail('');
+                setPassword('');
+                storeData(username);
+                navigation.navigate('Login');
+            } else {
+                throw new Error("An error has occurred");
+            }
+        } catch (error) {
+            alert("An error has occurred");
+            console.log(error);
+            setIsLoading(false);
+        }
+    };
+
     return (
         <SafeAreaView style={styles.container}>
             <StatusBar barStyle='light' backgroundColor={colors.PRIMARY} />
@@ -20,21 +81,46 @@ function Register({ navigation }) {
                 </Text>
                 <View style={styles.inputBox}>
                     <MaterialCommunityIcons name="account" size={30} color={colors.GRAY} />
-                    <TextInput style={styles.input} placeholder="Nom d'Utilisateur" />
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Nom d'Utilisateur"
+                        value={username}
+                        editable={!isLoading}
+                        onChangeText={onChangeNameHandler}
+                    />
                 </View>
                 <View style={styles.inputBox}>
                     <MaterialCommunityIcons name="email" size={30} color={colors.GRAY} />
-                    <TextInput style={styles.input} placeholder='Adresse Email' />
+                    <TextInput
+                        style={styles.input}
+                        placeholder='Adresse Email'
+                        value={email}
+                        editable={!isLoading}
+                        onChangeText={onChangeEmailHandler}
+                    />
                 </View>
                 <View style={styles.inputBox}>
                     <MaterialCommunityIcons name="lock" size={30} color={colors.GRAY} />
-                    <TextInput style={styles.input} placeholder='Mot de Passe' secureTextEntry />
+                    <TextInput style={styles.input}
+                        placeholder='Mot de Passe'
+                        secureTextEntry
+                        value={password}
+                        editable={!isLoading}
+                        onChangeText={onChangePasswordHandler}
+                    />
                 </View>
 
                 <TouchableOpacity
                     style={styles.action}
+                    onPress={onSubmitFormHandler}
+                    disabled={isLoading}
                 >
-                    <Text style={styles.actionText}>S'inscrire</Text>
+                    {isLoading ? (
+                        <ActivityIndicator size="large" color={colors.WHITE} />
+
+                    ) : (
+                        <Text style={styles.actionText}>S'inscrire</Text>
+                    )}
                 </TouchableOpacity>
                 <View style={styles.memberSection}>
                     <Text style={styles.memberText}>Deja membre ? </Text>
