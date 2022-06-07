c165e1b9b121ea754f0bb48411608572f70a3051
vanotis720 <vanotis720@gmail.com>
2022-06-07 08:58:20 +0200

    add back btn and add to kiff

--- src/components/page/Course.js
+++ src/components/page/Course.js
@@ -1,6 +1,8 @@
-import { StatusBar, StyleSheet, View, Text, ScrollView, Image } from 'react-native';
+import { StatusBar, StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import colors from '../../theme/colors';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
 
 function Course({ navigation }) {
     return (
@@ -12,6 +14,25 @@ function Course({ navigation }) {
                         source={require('../../assets/images/defaultAvatar.jpeg')}
                         style={styles.courseCover}
                     />
+                    <TouchableOpacity
+                        onPress={() => { navigation.goBack() }}
+                        style={{
+                            position: 'absolute',
+                            left: 5,
+                            top: 5,
+                        }} >
+                        <MaterialCommunityIcons name="arrow-left-circle" color={colors.PUREWHITE} size={30} />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => { alert('added to your favorite') }}
+                        style={{
+                            position: 'absolute',
+                            right: 5,
+                            top: 5,
+                        }} >
+                        <MaterialCommunityIcons name="cards-heart-outline" color={colors.PUREWHITE} size={30} />
+
+                    </TouchableOpacity>
                 </View>
                 <View style={styles.sectionTitle}>
                     <Text style={styles.title}>Le html c'est pour les null en vrai</Text>
