59bcd205c6ce4346bfcf8563c085a74b8aa2d4d9
vanotis720 <vanotis720@gmail.com>
2022-06-07 17:50:47 +0200

    home design ongoig

--- src/components/page/Home.js
+++ src/components/page/Home.js
@@ -1,4 +1,4 @@
-import { StatusBar, StyleSheet, ScrollView, View } from 'react-native';
+import { StatusBar, StyleSheet, ScrollView, View, Text, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import colors from '../../theme/colors';
 import CourseItem from './CourseItem';
@@ -8,7 +8,11 @@ function Home({ navigation }) {
         <SafeAreaView style={styles.container}>
             <StatusBar barStyle='light' backgroundColor={colors.PRIMARY} />
             <View style={styles.userPart}>
-
+                <Text style={styles.userTitle}>Welcom back, User</Text>
+                <Image
+                    source={require('../../assets/images/defaultAvatar.jpeg')}
+                    style={styles.userAvatar}
+                />
             </View>
             <ScrollView style={{ flex: 1 }}>
                 <CourseItem navigation={navigation} />
@@ -26,8 +30,22 @@ const styles = StyleSheet.create({
     },
     userPart: {
         flex: 1,
+        flexDirection: 'row',
+        marginHorizontal: 10
+    },
+    userAvatar: {
+        flex: 1,
+        height: 100,
+        width: 100,
+        borderRadius: 60,
+    },
+    userTitle: {
+        flex: 1,
+        fontSize: 24,
+        marginTop: 5,
+        marginBottom: 10
 
-    }
+    },
 });
 
 
